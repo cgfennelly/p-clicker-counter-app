16f337382a45f9f5aa12437e008532d261256cf4
Chris Fennelly <c.g.fennelly@gmail.com>
2025-07-27 17:01:35 +0000

    webhook fix wip

--- webhook-deploy.sh
+++ webhook-deploy.sh
@@ -4,6 +4,6 @@ echo "Starting deployment..."
 
 docker build -f Dockerfile.webhook -t webhook-listener-clicker .
 
-docker run -d -p 8076:8076 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/code/clicker-counter-app:/app -v /home/ubuntu/.ssh:/root/.ssh:ro --name webhook-listener-clicker webhook-listener-clicker
+docker run -d -p 8076:8076 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/code/p-clicker-counter-app:/app -v /home/ubuntu/.ssh:/root/.ssh:ro --name webhook-listener-clicker webhook-listener-clicker
 
 echo "Deployment finished."mk
